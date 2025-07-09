4bb1ed2873f9e09495b0ffc8fa73d7624ade619d
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-07-09 22:27:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+16. Simple Object and Accessing Properties
+
+let student = {
+  name: "Piyush",
+  age: 22,
+  course: "JavaScript"
+};
+
+console.log(student.name);  
+console.log(student["age"]); 
